47019e31eebf393305695658c52bfaa7bd5cde7d
sivagao <ghlndsl@126.com>
2016-02-17 13:44:22 +0800

    feat: add edit page and services

--- client/src/app/edit/edit.ts
+++ client/src/app/edit/edit.ts
@@ -1,18 +1,33 @@
 import {Component} from 'angular2/core';
 
+import {QuestionControlList} from './components/question-control-list';
+import {QuestionnaireOutline} from './components/questionnaire-outline';
+import {Questionnaire} from './components/questionnaire';
+
+import {DataService} from './data.service';
+
 console.log('`edit` page component loaded asynchronously');
 
 @Component({
   selector: 'edit-page',
-  template: require('./edit.html')
+  template: require('./edit.html'),
+  directives: [QuestionControlList, QuestionnaireOutline, Questionnaire],
+  providers: [DataService],
+  styles: [`
+    .tabs {
+      overflow: hidden;
+    }
+  `]
 })
 export class EditPage {
-  constructor() {
-
+  constructor(dataService: DataService) {
+    this.dataStore = dataService.data;
   }
 
   ngOnInit() {
     console.log('hello `Edit Page` component');
+    $('ul.tabs').tabs(); // Todo: specifc reference
   }
 
 }
+
